252fb363843ef60e46c953051b92c55ffcb98a26
alexwen.creates@gmail.com <alexwen.creates@gmail.com>
2025-05-06 20:03:14 -0500

    Add netlify.

---
+++ .netlify/functions-internal/___netlify-server-handler/.next/server/next-font-manifest.js
@@ -0,0 +1,1 @@
+self.__NEXT_FONT_MANIFEST='{"pages":{},"app":{"/Users/alexwen/Projects/sixty-next/src/app/layout":["static/media/93f479601ee12b01-s.p.woff2","static/media/61b81e7bde89ba40-s.p.woff2"]},"appUsingSizeAdjust":true,"pagesUsingSizeAdjust":false}';
