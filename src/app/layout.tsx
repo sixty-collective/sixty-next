368e9d75baa6e66d76b1ccab50db00ae8925362d
alexwen.creates@gmail.com <alexwen.creates@gmail.com>
2025-05-27 13:07:04 -0500

    Import Poppins.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,14 @@
 import Script from "next/script";
-import { Nunito_Sans, Geist_Mono } from "next/font/google";
+import { Nunito_Sans, Geist_Mono, Poppins } from "next/font/google";
 import "./globals.css";
 import "../styles/global.css"
 import Layout from "../components/layout"
+
+const poppins = Poppins({
+  weight: ['300', '400', '500', '600', '700', '800'],
+  variable: "--font-poppins",
+  subsets: ['latin'],
+})
 
 const nunitoSans = Nunito_Sans({
   variable: "--font-nunito-sans",
@@ -20,10 +26,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html suppressHydrationWarning lang="en">
+    <html suppressHydrationWarning lang="en" className={`${poppins.variable} ${nunitoSans.variable} ${geistMono.variable} antialiased`}>
       <body
       suppressHydrationWarning
-        className={`${nunitoSans.variable} ${geistMono.variable} antialiased`}
       >
         <Layout>
           <Script src="https://kit.fontawesome.com/224444bc09.js"></Script>
